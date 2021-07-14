ac9b0396e02d9c76708b69d9d05fb04ec3555082
Kazi Hasan Ibn Arif EFAZ <efaz701815@gmail.com>
2021-07-15 00:20:06 +0600

    show our bone

--- src/main/scala/example/ExampleApp.scala
+++ src/main/scala/example/ExampleApp.scala
@@ -19,6 +19,16 @@ import scalismo.common.interpolation.TriangleMeshInterpolator3D
 import scalismo.ui.api._
 import scalismo.io.MeshIO
 
+// show
+
+import scalismo.mesh.TriangleMesh
+import scalismo.io.{MeshIO, StatisticalModelIO}
+import scalismo.common.PointId
+import scalismo.mesh.TriangleId
+import scalismo.geometry.{_3D, Point3D}
+import scalismo.image.{DiscreteImage, DiscreteImage3D}
+import scalismo.statisticalmodel.PointDistributionModel // indicates that we work in 3D space
+
 
 object ExampleApp {
 
@@ -29,14 +39,30 @@ object ExampleApp {
 
       val ui = ScalismoUI()
 
-      ////////////////////// add code 
-      
-      val mesh: TriangleMesh[_3D] = MeshIO.readMesh(new java.io.File("datasets/femur.ply")).get
-      val mesh2: TriangleMesh[_3D] = MeshIO.readMesh(new java.io.File("datasets/femur_head.ply")).get
+      ////////////////////// add code  : gauss
 
-      val paolaGroup = ui.createGroup("femur")
-      ui.show(paolaGroup, mesh, "femur")
-      ui.show(paolaGroup, mesh2, "femur head")
+      val mesh: TriangleMesh[_3D] = MeshIO.readMesh(new java.io.File("datasets/Segmentation_Segment_2.stl")).get
+      
+      val femur = ui.createGroup("femur")
+      val meshView = ui.show(femur, mesh, "femur")
+
+      println("first point " + mesh.pointSet.point(PointId(0)))
+      println("first point " + mesh.pointSet.point(PointId(1)))
+      println("first point " + mesh.pointSet.point(PointId(2)))
+
+// first point Point3D(162.2697296142578,-11.115056991577148,301.18719482421875)
+     
+     
+     
+     
+     // basicval pointCloudView = ui.show(femur, mesh.pointSet, "pointCloud")
+
+
+
+
+
+
+      
     
       
   }
